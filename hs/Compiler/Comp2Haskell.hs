628f5b708d1048c6e6df37847c061fedc1374a76
Benjamin Teuber <4teuber@informatik.uni-hamburg.de>
2009-03-24 20:42:01 +0100

    working on Comp2Haskell

--- hs/Compiler/Comp2Haskell.hs
+++ hs/Compiler/Comp2Haskell.hs
@@ -6,10 +6,9 @@ import Arrows
 import Sexp
 import Parser
 import Compiler
-import qualified Haskell2Code as H
 
-quote :: SexpParser () Sexp
-quote = macro "'" inner
+compQuote :: SexpParser () Sexp
+compQuote = macro "'" inner
     where inner                  = unquote <+> procSymbol <+> procNode
           procSymbol             = takeSymbol >>> arr quoteSymbol
           procNode               = takeAnySexp (arr snd &&& (voidArrow >>> inners)) >>> arr quoteSexp
@@ -19,4 +18,26 @@ quote = macro "'" inner
           quoteSymbol            = singleNode "symbol" . singleNode "str" . symbol
           quoteSexp (lbl, inner) = Sexp "Sexp" [singleNode "str" (symbol lbl), inner]
 
-compiler = macro "compiler"
+compMac :: SexpParser () Sexp
+compMac = macro "mac" ((takeSymbol &&& take &&& optional compWhere) >>> buildMac)
+    where compWhere
+    where buildMac (name, (body, whereClause)) =
+              Sexp "=" ([symbol name,
+                         Sexp "macro" [singleNode "str" name,
+                                       body]]
+                        ++ whereClause
+
+compiler = macro "compiler" ((takeSymbol &&& many compMac) >>> buildCompiler)
+    where buildcompiler (name, macs) = 
+              (Sexp "haskell" (Sexp "module" [name, 
+                                              Sexp "Prelude" [Sexp "hiding" [lines, words, take]],
+                                              symbol "Control.Arrow",
+                                              symbol "Util",
+                                              symbol "Arrows",
+                                              symbol "Sexp",
+                                              symbol "Parser",
+                                              symbol "Compiler"]
+                               : macs))
+                               
+                               
+                                              
