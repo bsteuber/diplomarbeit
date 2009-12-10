65367bc866123883774e85a4ced38e48f1e77e39
Benjamin Teuber <bsteuber@gmail.com>
2009-12-10 12:47:34 +0100

    renamed mac to autoMac

--- hs/Compiler/Comp2Haskell.hs
+++ hs/Compiler/Comp2Haskell.hs
@@ -10,31 +10,31 @@ import Sexp
 import Parser
 import Model
 
-regMacs :: (IORef [Sexp])
+regAutoMacs :: (IORef [Sexp])
 
-regMacs = (unsafePerformIO (newIORef []))
+regAutoMacs = (unsafePerformIO (newIORef []))
 
-addMac = (ioToParser (\ m -> (do
-  (modifyIORef regMacs (\ ms -> (m : ms)))
+addAutoMac = (ioToParser (\ m -> (do
+  (modifyIORef regAutoMacs (\ ms -> (m : ms)))
   (return m))))
 
-getMacs = (ioToParser (const (readIORef regMacs)))
+getAutoMacs = (ioToParser (const (readIORef regAutoMacs)))
 
 compExpandCompiler :: LispMacro
 
 compExpandCompiler = (macro "expandCompiler" (liftA2 gen (compSymbol >>> (id &&& (arr symbolToLower))) (many take)))
   where
-    gen (name, lowerName) defs = ([node ([symbol "module"] ++ [name] ++ [node ([symbol "Prelude"] ++ [node ([symbol "hiding"] ++ [symbol "id"] ++ [symbol "lines"] ++ [symbol "words"] ++ [symbol "take"])])] ++ [symbol "Data.IORef"] ++ [node ([symbol "Control.Category"] ++ [node ([symbol "only"] ++ [symbol "id"])])] ++ [symbol "Control.Arrow"] ++ [symbol "System.IO.Unsafe"] ++ [symbol "Util"] ++ [symbol "Arrows"] ++ [symbol "Sexp"] ++ [symbol "Parser"] ++ [symbol "Model"])] ++ defs ++ [node ([symbol "hasType"] ++ [lowerName] ++ [symbol "LispMacro"])] ++ [node ([symbol "="] ++ [lowerName] ++ [node ([symbol "simpleTraverse"] ++ [node ([symbol "allMacs"])])])])
+    gen (name, lowerName) defs = ([node ([symbol "module"] ++ [name] ++ [node ([symbol "Prelude"] ++ [node ([symbol "hiding"] ++ [symbol "id"] ++ [symbol "lines"] ++ [symbol "words"] ++ [symbol "take"])])] ++ [symbol "Data.IORef"] ++ [node ([symbol "Control.Category"] ++ [node ([symbol "only"] ++ [symbol "id"])])] ++ [symbol "Control.Arrow"] ++ [symbol "System.IO.Unsafe"] ++ [symbol "Util"] ++ [symbol "Arrows"] ++ [symbol "Sexp"] ++ [symbol "Parser"] ++ [symbol "Model"])] ++ defs ++ [node ([symbol "hasType"] ++ [lowerName] ++ [symbol "LispMacro"])] ++ [node ([symbol "="] ++ [lowerName] ++ [node ([symbol "simpleTraverse"] ++ [node ([symbol "allAutoMacs"])])])])
 
-compAllMacs :: LispMacro
+compAllAutoMacs :: LispMacro
 
-compAllMacs = (macro "allMacs" (getMacs >>^ listMacs))
+compAllAutoMacs = (macro "allAutoMacs" (getAutoMacs >>^ listAutoMacs))
   where
-    listMacs ms = ([node ([symbol "List"] ++ ms)])
+    listAutoMacs ms = ([node ([symbol "List"] ++ ms)])
 
-compMac :: LispMacro
+compAutoMac :: LispMacro
 
-compMac = (macro "mac" (liftA4 gen (compSymbol >>> addMac) compSymbol take (many take)))
+compAutoMac = (macro "autoMac" (liftA4 gen (compSymbol >>> addAutoMac) compSymbol take (many take)))
   where
     gen fun sym cmd cmds = ([node ([symbol "hasType"] ++ [fun] ++ [symbol "LispMacro"])] ++ [node ([symbol "="] ++ [fun] ++ [node ([symbol "macro"] ++ [node ([symbol "Str"] ++ [sym])] ++ [cmd])] ++ cmds)])
 
@@ -44,7 +44,7 @@ genQuotes = (macro "quotes" (constArrow qts))
   where
     qts = (concatMap (genQt . mkSyms) ["", "1", "2", "3"])
     mkSyms s = (map (symbol . (\ str -> (str ++ s))) ["compQuote", "'", ",", ",@"])
-    genQt [name, symQuote, symUnquote, symUnquoteAll] = ([node ([symbol "mac"] ++ [name] ++ [symQuote] ++ [node ([symbol ">>^"] ++ [symbol "inners"] ++ [symbol "single"])] ++ [node ([symbol "where"] ++ [node ([symbol "="] ++ [symbol "inner"] ++ [node ([symbol "<+>"] ++ [symbol "unquote"] ++ [symbol "procSymbol"] ++ [symbol "procNode"])])] ++ [node ([symbol "="] ++ [symbol "procSymbol"] ++ [node ([symbol ">>>"] ++ [symbol "takeSymbol"] ++ [node ([symbol "arr"] ++ [symbol "quoteSymbol"])])])] ++ [node ([symbol "="] ++ [symbol "procNode"] ++ [node ([symbol ">>>"] ++ [node ([symbol "compNode"] ++ [symbol "inners"])] ++ [node ([symbol "arr"] ++ [symbol "quoteNode"])])])] ++ [node ([symbol "="] ++ [symbol "inners"] ++ [node ([symbol ">>>"] ++ [node ([symbol "many"] ++ [node ([symbol "<+>"] ++ [symbol "unquoteAll"] ++ [node ([symbol ">>>"] ++ [symbol "inner"] ++ [node ([symbol "arr"] ++ [node ([symbol "namedNode"] ++ [node ([symbol "Str"] ++ [symbol "List"])])])])])])] ++ [node ([symbol "arr"] ++ [node ([symbol "namedNode"] ++ [node ([symbol "Str"] ++ [symbol "++"])])])])])] ++ [node ([symbol "="] ++ [symbol "unquote"] ++ [node ([symbol "macro"] ++ [node ([symbol "Str"] ++ [symUnquote])] ++ [node ([symbol ">>^"] ++ [symbol "take"] ++ [symbol "single"])])])] ++ [node ([symbol "="] ++ [symbol "unquoteAll"] ++ [node ([symbol "macro"] ++ [node ([symbol "Str"] ++ [symUnquoteAll])] ++ [symbol "take"])])] ++ [node ([symbol "="] ++ [node ([symbol "quoteSymbol"] ++ [symbol "str"])] ++ [node ([symbol "'1"] ++ [node ([symbol "symbol"] ++ [node ([symbol "Str"] ++ [node ([symbol ",1"] ++ [node ([symbol "symbol"] ++ [symbol "str"])])])])])])] ++ [node ([symbol "="] ++ [node ([symbol "quoteNode"] ++ [symbol "nod"])] ++ [node ([symbol "'1"] ++ [node ([symbol "node"] ++ [node ([symbol ",1"] ++ [symbol "nod"])])])])])])])
+    genQt [name, symQuote, symUnquote, symUnquoteAll] = ([node ([symbol "autoMac"] ++ [name] ++ [symQuote] ++ [node ([symbol ">>^"] ++ [symbol "inners"] ++ [symbol "single"])] ++ [node ([symbol "where"] ++ [node ([symbol "="] ++ [symbol "inner"] ++ [node ([symbol "<+>"] ++ [symbol "unquote"] ++ [symbol "procSymbol"] ++ [symbol "procNode"])])] ++ [node ([symbol "="] ++ [symbol "procSymbol"] ++ [node ([symbol ">>>"] ++ [symbol "takeSymbol"] ++ [node ([symbol "arr"] ++ [symbol "quoteSymbol"])])])] ++ [node ([symbol "="] ++ [symbol "procNode"] ++ [node ([symbol ">>>"] ++ [node ([symbol "compNode"] ++ [symbol "inners"])] ++ [node ([symbol "arr"] ++ [symbol "quoteNode"])])])] ++ [node ([symbol "="] ++ [symbol "inners"] ++ [node ([symbol ">>>"] ++ [node ([symbol "many"] ++ [node ([symbol "<+>"] ++ [symbol "unquoteAll"] ++ [node ([symbol ">>>"] ++ [symbol "inner"] ++ [node ([symbol "arr"] ++ [node ([symbol "namedNode"] ++ [node ([symbol "Str"] ++ [symbol "List"])])])])])])] ++ [node ([symbol "arr"] ++ [node ([symbol "namedNode"] ++ [node ([symbol "Str"] ++ [symbol "++"])])])])])] ++ [node ([symbol "="] ++ [symbol "unquote"] ++ [node ([symbol "macro"] ++ [node ([symbol "Str"] ++ [symUnquote])] ++ [node ([symbol ">>^"] ++ [symbol "take"] ++ [symbol "single"])])])] ++ [node ([symbol "="] ++ [symbol "unquoteAll"] ++ [node ([symbol "macro"] ++ [node ([symbol "Str"] ++ [symUnquoteAll])] ++ [symbol "take"])])] ++ [node ([symbol "="] ++ [node ([symbol "quoteSymbol"] ++ [symbol "str"])] ++ [node ([symbol "'1"] ++ [node ([symbol "symbol"] ++ [node ([symbol "Str"] ++ [node ([symbol ",1"] ++ [node ([symbol "symbol"] ++ [symbol "str"])])])])])])] ++ [node ([symbol "="] ++ [node ([symbol "quoteNode"] ++ [symbol "nod"])] ++ [node ([symbol "'1"] ++ [node ([symbol "node"] ++ [node ([symbol ",1"] ++ [symbol "nod"])])])])])])])
 
 compQuote :: LispMacro
 
@@ -100,4 +100,4 @@ compQuote3 = (macro "'3" (inners >>^ single))
 
 comp2haskell :: LispMacro
 
-comp2haskell = (simpleTraverse [compQuote3, compQuote2, compQuote1, compQuote, genQuotes, compMac, compAllMacs, compExpandCompiler])
+comp2haskell = (simpleTraverse [compQuote3, compQuote2, compQuote1, compQuote, genQuotes, compAutoMac, compAllAutoMacs, compExpandCompiler])
